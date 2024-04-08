e41d4bae81918b2bf1436681d23abff84fafb427
mossener <2325269334@qq.com>
2024-04-08 21:27:43 +0800

    3

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -15,8 +15,7 @@ class ThreadPool {
 public:
     ThreadPool(size_t);
     template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) 
-        -> std::future<typename std::result_of<F(Args...)>::type>;
+    auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>;
     ~ThreadPool();
 private:
     // need to keep track of threads so we can join them
